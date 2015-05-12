a5535e89286d38403dc0c8e5c677e472576e2fd4
leon486@gmail.com <leon486@gmail.com>
2015-05-12 10:31:30 -0400

    removed comments in files

--- 3-2/src/_3_2Game_Server/Server.java
+++ 3-2/src/_3_2Game_Server/Server.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package _3_2Game_Server;
 
 import _3_2Game.UI_cmd;
 import java.io.IOException;
 import java.net.ServerSocket;
 
-/**
- *
- * @author ivantactukmercado
- */
 public class Server {
 
 	public static void main(String[] args) throws IOException {
